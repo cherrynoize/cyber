81e687b85d4aa0a309f3255fec1a4aeba6c3c56d
cherrynoize <cherrynoize9987@outlook.com>
2023-05-12 16:52:32 +0200

    fixed divergent styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <footer>
-      <a href="https://cherrynoize.github.io/contacts">
-        Contacts
-      </a>
+      <Link to='/contacts'>
+        <button className='a'>
+          Contacts
+        </button>
+      </Link>
+      <Link to='/contribute'>
+        <button className='a'>
+          Contribute
+        </button>
+      </Link>
       <span>
         &copy; All rights reserved 2023
       </span>
