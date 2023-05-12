76001445606f2c5d00ca4271dd081896f933488a
cherrynoize <cherrynoize9987@outlook.com>
2023-05-12 16:55:30 +0200

    fixed home url

--- src/Main.jsx
+++ src/Main.jsx
@@ -12,11 +12,11 @@ const Main = () => {
   return (
     <main>
       <Routes>
-        <Route path='/gallery' element={<Home/>} />
-        <Route path='/gallery/cyber' element={<Cyber/>} />
-        <Route path='/gallery/dreams' element={<Dreams/>} />
-        <Route path='/gallery/glitch' element={<Glitch/>} />
-        <Route path='/gallery/misc' element={<Misc/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='gallery/cyber' element={<Cyber/>} />
+        <Route path='gallery/dreams' element={<Dreams/>} />
+        <Route path='gallery/glitch' element={<Glitch/>} />
+        <Route path='gallery/misc' element={<Misc/>} />
       </Routes>
     </main>
   );
